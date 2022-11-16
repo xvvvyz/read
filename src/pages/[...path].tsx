e93e765c00a2fa2498bdec0f899ebcd727c12048
Cade Scroggins <git@cade.me>
2022-11-16 15:49:21 -0800

    update fonts and layout

--- src/pages/[...path].tsx
+++ src/pages/[...path].tsx
@@ -5,22 +5,15 @@ import rehypeSlug from 'rehype-slug';
 import remarkGemoji from 'remark-gemoji';
 import remarkGfm from 'remark-gfm';
 import { GetStaticPropsContext } from 'next';
-import { Inter, Roboto_Mono } from '@next/font/google';
 import { NextSeo } from 'next-seo';
 import getPath from '../get-path';
-
-const inter = Inter({ subsets: ['latin'] });
-const roboto = Roboto_Mono({ subsets: ['latin'] });
 
 const ReadMePage = ({ markdown, pathParts }: { markdown: string; pathParts: string[] }) => (
   <>
     <NextSeo title={pathParts.slice(-1)[0]} />
     <ReactMarkdown
-      className={inter.className}
       components={{
-        code: ({ children, inline }) => (
-          <code className={`${roboto.className} ${inline ? 'inline' : ''}`}>{children}</code>
-        ),
+        code: ({ children, inline }) => <code className={inline ? 'inline' : ''}>{children}</code>,
         link: ({ children, href, title }) => (
           <Link href={href ?? '#'} title={title}>
             {children}
